f98a7c1c6c2054ba325ab760a319d0c02c25e915
Muhammad Asif <asif88383@gmail.com>
2022-04-05 15:42:21 +0500

    first commit

--- setup/src/App.js
+++ setup/src/App.js
@@ -1,8 +1,22 @@
 import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
+
+
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  return (
+    <main>
+      <div className='container'>
+        <h3>FAQ's about Login</h3>
+        <section className='info'>
+          {currentQuestion.map((question) => {
+            return <SingleQuestion key={question.id} {...question} />;
+          })}
+        </section>  
+      </div>
+    </main>
+  )
 }
 
 export default App;
